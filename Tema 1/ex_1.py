4fdff8bdbda1151e7fa0fa5395d5747a95821257
TOMA Victor-Stefan <viky.toma15@gmail.com>
2024-03-05 14:13:23 +0200

    .

--- Tema 1/ex_1.py
+++ Tema 1/ex_1.py
@@ -1,8 +1,9 @@
-def main():
+def main ():
     p = 1
     while (1 + pow(10, -p)) != 1:
         p += 1
-    p-1
+    p = p - 1
     print(pow(10, -p))
+
 
 main()
--- Tema 1/ex_2.py
+++ Tema 1/ex_2.py
@@ -1,15 +1,16 @@
-def ex1():
+def ex1 ():
     p = 1
     while (1 + pow(10, -p)) != 1:
         p += 1
-    p=p-1
+    p = p - 1
     return pow(10, -p)
 
-def main():
+
+def main ():
     u = ex1()
-    x=1.0
-    y=u/10
-    z=u/10
+    x = 1.0
+    y = u / 10
+    z = u / 10
     left = (x + y) + z
     right = x + (y + z)
     if left == right:
@@ -17,15 +18,15 @@ def main():
     else:
         print("left != right")
 
-    x=0.1
-    y=0.2
-    z=0.3
+    x = 0.1
+    y = 0.2
+    z = 0.3
     left = (x * y) * z
     right = x * (y * z)
     if left == right:
         print("left == right")
     else:
         print("left != right")
-    
+
 
 main()
